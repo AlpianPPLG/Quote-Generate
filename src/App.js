168365d5981ac2eb4d80417a63aa24d1964258a8
Alpian <Nova07pplg@gmail.com>
2024-05-28 20:39:25 +0800

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  const handleGetAdvice = async () => {
+    try {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      const { slip } = response.data;
+
+      setAdvice(slip.advice);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      <h1 className="app__title" onClick={() => {window.location.reload();}}>Advice Generator</h1>
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={handleGetAdvice}>
+          <span>Give Me Advice</span>
+        </button>
+      </div>
+      <div className="card__footer">
+        <p className="footer__text">
+          Made with love by{" "}
+          <a
+            href="https://github.com/AlpianPPLG"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            AlpianPPLG
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
